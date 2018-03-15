c27d65db94da4ff3da65da6ab985955534b745d8
Rehan Parkar <parkarcamp@gmail.com>
2018-03-14 19:37:25 -0500

    location services added and centre location on map

--- MapLocationCollections/MapViewController.swift
+++ MapLocationCollections/MapViewController.swift
@@ -8,24 +8,59 @@
 
 import UIKit
 import MapKit
+import CoreLocation
+
 
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManger = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManger.delegate = self
+        configureLocationService()
 
     }
 
     
     @IBAction func centerMapButtonPressed(_ sender: Any) {
+        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapViewController: MKMapViewDelegate {
     
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManger.location?.coordinate else {return}
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func configureLocationService(){
+        
+        if authorizationStatus == .notDetermined {
+            locationManger.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
