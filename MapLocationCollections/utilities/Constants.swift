c1b49ece6c83c1afd78c3186a2fe8984e0553d3b
Rehan Parkar <parkarcamp@gmail.com>
2018-03-15 20:13:13 -0500

    added API

---
+++ MapLocationCollections/utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MapLocationCollections
+//
+//  Created by Rehan Parkar on 2018-03-15.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "548335eec518c65ada3111b3f1bcb4bd"
+
+func flickrURL(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int ) -> String{
+    
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+    
+}
